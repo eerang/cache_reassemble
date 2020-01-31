abfbaca3eba4f07842c427209f1bf7730567532b
yr <yirang0329@gmail.com>
2020-01-31 16:11:15 +0900

    Update echo_url_extract.py

--- echo_url_extract.py
+++ echo_url_extract.py
@@ -21,7 +21,7 @@ def csv_write(path, _list):
 
 if __name__ == '__main__':
 
-    basepath = "D:\\0.PYTHON\\Cache\\test\\org.mozilla.connect.firefox\\cache\\org.chromium.android_webview"
+    basepath = "\\org.chromium.android_webview"
     data = []
     for file_name in os.listdir(basepath) :
         target = os.path.join(basepath, file_name)
@@ -29,26 +29,3 @@ if __name__ == '__main__':
     csv_write("firefox_test.csv",data)
 
 
-
-
-
-
-
-
-
-
-
-
-'''
-def get_url(path):
-    cs = sr.cref(["magic","version","urllength","md5"],[8,4,4,4],[False,False,False,False],None,"Cache_Header") << sr.cref.structure
-    with  open(str(path), 'rb') as file:
-        buffer = file.read(1024)
-        if(buffer==b''):
-            print("EOF")
-            sys.exit()
-    cs << buffer
-    length = cs.byte2int((cs >> ("urllength",)))
-    return buffer[cs.sizeof()+4:cs.sizeof()+4+length].decode('utf-8').strip()
-
-'''
